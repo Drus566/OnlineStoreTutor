12def4beb95c4bdb2cbfeb0e342845488e3e581e
Drus566 <ahdpeu566@mail.ru>
2018-11-04 21:41:02 +0500

    styles

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
     validates :title, :description, :image_url, presence: true
     validates :title, uniqueness: true
+    validates :title, length: { maximum: 10 }
     validates :price, numericality: { greather_than_or_equal_to: 0.01 }
     validates :image_url, allow_blank: true, format: {
         with: %r{\.(gif|jpg|png)\Z}i,
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Product.delete_all
 # . . .
-Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
+Product.create!(title: 'MobileApps',
     description:
         %{<p>
         <em>Native Apps, Multiple Platforms</em>
@@ -13,7 +13,20 @@ Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
     image_url: '7apps.jpg',
     price: 26.00)
     
-Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
+Product.create!(title: 'MobApps',
+    description:
+        %{<p>
+        <em>Native Apps, Multiple Platforms</em>
+        Answer the question “Can we build this for ALL the devices?” with a
+        resounding YES. This book will help you get there with a real-world
+        introduction to seven platforms, whether you’re new to mobile or an
+        experienced developer needing to expand your options. Plus, you’ll find
+        out which cross-platform solution makes the most sense for your needs.
+        </p>},
+    image_url: '7apps.jpg',
+    price: 26.00)
+
+Product.create!(title: 'Seven',
     description:
         %{<p>
         <em>Native Apps, Multiple Platforms</em>
